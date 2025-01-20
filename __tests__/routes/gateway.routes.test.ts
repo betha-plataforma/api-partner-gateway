dc0107d94f9afb14b6862b81d9651964cbfd6078
Lucas de Castro Zanoni <lucas.zanoni@betha.com.br>
2025-01-20 04:28:22 -0300

    feat: jwt validations

--- __tests__/routes/gateway.routes.test.ts
+++ __tests__/routes/gateway.routes.test.ts
@@ -1,13 +1,78 @@
 import request from "supertest";
-
+import * as jose from "node-jose";
 import app from "../../src/app";
 
+/**
+ * Tests for the gateway routes.
+ */
 describe("Gateway routes", () => {
+    let keystore: jose.JWK.KeyStore;
+    let key: jose.JWK.Key;
+
+    /**
+     * Generate a key before running the tests.
+     */
+    beforeAll(async () => {
+        keystore = jose.JWK.createKeyStore();
+        key = await keystore.generate("oct", 256, { alg: "HS256", use: "sig" });
+    });
 
     test("Get gateway", async () => {
-        const res = await request(app).post("/partner-gateway/v1/auth");
+        const payload = { userId: "12345", role: "admin" };
+        const token = await jose.JWS.createSign(
+            { format: "compact", fields: { typ: "JWT" } },
+            key
+        )
+            .update(JSON.stringify(payload))
+            .final();
+
+        const res = await request(app)
+            .post("/partner-gateway/v1/auth")
+            .send({ token });
+
         expect(res.text).toEqual("redirected");
         expect(res.status).toBe(200);
     });
 
+    test("Get gateway with missing token", async () => {
+        const res = await request(app)
+            .post("/partner-gateway/v1/auth");
+
+        expect(res.status).toBe(422);
+        expect(res.body).toEqual({
+            errors: [
+                {
+                    type: "field",
+                    path: "token",
+                    msg: "Invalid JWT token",
+                    location: "body"
+                },
+                {
+                    location: "body",
+                    msg: "Invalid JWT token",
+                    path: "token",
+                    type: "field"
+                }
+            ]
+        });
+    });
+
+    test("Get gateway with invalid token", async () => {
+        const res = await request(app)
+            .post("/partner-gateway/v1/auth")
+            .send({ token: "invalid-token" });
+
+        expect(res.status).toBe(422);
+        expect(res.body).toEqual({
+            errors: [
+                {
+                    type: "field",
+                    msg: "Invalid JWT token",
+                    path: "token",
+                    location: "body",
+                    value: "invalid-token"
+                }
+            ]
+        });
+    });
 });
--- src/gateway/gateway.cotroller.ts
+++ src/gateway/gateway.cotroller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { body, check, validationResult } from "express-validator";
 
 /**
  * Controller class for handling gateway-related functionality.
@@ -24,8 +25,28 @@ class GatewayController {
     /**
      * Authenticates and redirects the user to the application.
      */
-    public auth(): void {
+    public async auth(): Promise<void> {
+        await check("token", "Invalid JWT token").exists().isJWT().run(this.req);
+        if (!this.handleValidationErrors()) return;
+
         this.res.status(200).send("redirected");
+    }
+
+    /**
+     * Validates the request and handles errors.
+     * If there are validation errors, it sends a 422 response.
+     *
+     * @returns {boolean} Returns `true` if no validation errors, `false` otherwise.
+     */
+    private handleValidationErrors(): boolean {
+        const errors = validationResult(this.req);
+
+        if (!errors.isEmpty()) {
+            this.res.status(422).json({ errors: errors.array() });
+            return false;
+        }
+
+        return true;
     }
 }
 
