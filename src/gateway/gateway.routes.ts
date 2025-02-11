d16091861146d568f716090ad2c6a66a01e4b9a6
Lucas de Castro Zanoni <lucas.zanoni@betha.com.br>
2025-02-11 19:47:14 -0300

    fix: gateway router para evitar a criacao dos recursos para cada requisicao

--- src/gateway/gateway.routes.ts
+++ src/gateway/gateway.routes.ts
@@ -5,15 +5,12 @@ import { AuthImpl } from './auth/auth.impl.js';
 import { CachingAuthProvider } from './auth/auth.cache.decorator.js';
 
 const router = Router();
+const gatewayService = new GatewayService();
+const authProvider = new CachingAuthProvider(new AuthImpl());
+const gatewayController = new GatewayController(gatewayService, authProvider);
 
 /**
  * Routes for the gateway
  */
-router.all('*', (req: Request, res: Response, next: any) =>
-    new GatewayController(new GatewayService(), new CachingAuthProvider(new AuthImpl())).auth(
-        req,
-        res,
-        next
-    )
-);
+router.all('*', (req: Request, res: Response, next: any) => gatewayController.auth(req, res, next));
 export { router };
